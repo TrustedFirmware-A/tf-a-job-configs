92151adbac5695691e770ec8e569457cce6aa3cd
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2024-02-26 12:38:32 +0700

    tf-a-lts-docs-watcher.sh: Consistently use spaces for indentation
    Run thru "expand" utility.
    
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>
    Change-Id: Ifd670d9988975a4c7bd9d312944a2a80ab3deffa

--- scripts/tf-a-lts-docs-watcher.sh
+++ scripts/tf-a-lts-docs-watcher.sh
@@ -46,9 +46,9 @@ function activate_version() {
 echo "Notifying ReadTheDocs of changes on: ${lts_branch}"
 build_trigger=$(curl -s -X POST -d "branches=${lts_branch}" -d "token=${RTD_WEBHOOK_SECRET_KEY}" ${RTD_WEBHOOK_URL} | jq .build_triggered)
 if [ "${build_trigger}" = "false" ]; then
-	# The branch might be new and hasn't been known by RTD, or hasn't been activated, or both
+        # The branch might be new and hasn't been known by RTD, or hasn't been activated, or both
     # we can trigger a build for the master branch to update all branches
-	echo "The branch ${lts_branch} is now! Activate and hide it!"
+        echo "The branch ${lts_branch} is now! Activate and hide it!"
     curl -s -X POST -d "branches=master" -d "token=${RTD_WEBHOOK_SECRET_KEY}" ${RTD_WEBHOOK_URL}
     activate_version ${lts_branch}
     curl -s -X PATCH -H "Content-Type: application/json" -H "Authorization: Token ${RTD_API_TOKEN}" \
@@ -60,12 +60,12 @@ if [ -n "${new_tag}" ]; then
     echo -e "\nNew release tag: ${new_tag}"
     # Hide the current active and unhidden tags
     old_tags=$(curl -s -H "Authorization: Token ${RTD_API_TOKEN}" "${RTD_VER_API}/?slug=${lts_branch}&type=tag&active=true" | \
-    		   jq -r '.results | map(select(.hidden == false) | .verbose_name) | .[]')
+                   jq -r '.results | map(select(.hidden == false) | .verbose_name) | .[]')
     for t in ${old_tags};
     do
         echo "Hide old tag: ${t}"
         curl -s -X PATCH -H "Content-Type: application/json" -H "Authorization: Token ${RTD_API_TOKEN}" \
-        	 -d "{\"hidden\": true}" ${RTD_VER_API}/${t}/
+                 -d "{\"hidden\": true}" ${RTD_VER_API}/${t}/
     done
     # Active the new version
     echo "Active new version: ${new_tag}"
