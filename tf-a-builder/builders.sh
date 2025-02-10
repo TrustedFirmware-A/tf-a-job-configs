24fcaf9ba30d77aa96b4b294f5573eec0b8e0036
Saheer Babu <saheer.babu@arm.com>
2025-02-10 11:01:44 +0000

    Use environment variable for download server
    To ensure tf-a-jobs can run on both OpenCI
    and Arm CI, replace ci.trustedfirmware.org with
    enironment variable.
    
    DOWNLOAD_SERVER_URL is a system-wide environment
    variable and was configured in:
    Manage Jenkins > System > Global Properties
    
    Signed-off-by: Saheer Babu <saheer.babu@arm.com>
    
    Change-Id: Ibbb7bf98c087e12617b14481cfa0e5a848cf8c8e

--- tf-a-builder/builders.sh
+++ tf-a-builder/builders.sh
@@ -60,7 +60,7 @@ export mbedtls_archive=${WORKSPACE}/nfs/downloads/mbedtls/$(ls -1 mbedtls-*.tar.
 # Path to root of CI repository
 ci_root="${WORKSPACE}/tf-a-ci-scripts"
 
-export tf_downloads="https://downloads.trustedfirmware.org"
+export tf_downloads="${DOWNLOAD_SERVER_URL}"
 export tfa_downloads="${tf_downloads}/tf-a"
 
 # Fetch required firmware/binaries and place it at proper location
