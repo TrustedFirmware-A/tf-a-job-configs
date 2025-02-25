19387ac073148f05d28a3b439c1a0714c213b374
Saheer Babu <saheer.babu@arm.com>
2025-02-25 16:00:17 +0000

    User ENV variable for TuxPub bucket
    To help migration to Arm OpenCI, use ENV variable and set
    this is jenkins system variables.
    
    Change-Id: I63068e6d9ae9f1d43d445a2e4aad6b74e8ba8674
    Signed-off-by: Saheer Babu <saheer.babu@arm.com>

--- tf-a-builder/upload-tfa-next-artifacts.sh
+++ tf-a-builder/upload-tfa-next-artifacts.sh
@@ -7,6 +7,6 @@ pip install -U tuxput
 # Use tpcli and the "upload" URL to upload these artifacts to the S3 bucket.
 # tpcli seems to only accept files that are in the current directory, therefore cd into artefacts-lava.
 cd ${WORKSPACE}/artefacts-lava
-/home/buildslave/.local/bin/tpcli -t ${TUXPUT_ARCHIVE_TOKEN} -b trustedfirmware-prod-storage "$URL_FIP" fip.bin
-/home/buildslave/.local/bin/tpcli -t ${TUXPUT_ARCHIVE_TOKEN} -b trustedfirmware-prod-storage "$URL_BL1" bl1.bin
+/home/buildslave/.local/bin/tpcli -t ${TUXPUT_ARCHIVE_TOKEN} -b ${TUXPUB_S3_BUCKET} "$URL_FIP" fip.bin
+/home/buildslave/.local/bin/tpcli -t ${TUXPUT_ARCHIVE_TOKEN} -b ${TUXPUB_S3_BUCKET} "$URL_BL1" bl1.bin
 cd -
