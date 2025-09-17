5deabd8f85c294b953d3cca9dbbb7945edaf5126
Arthur She <arthur.she@linaro.org>
2025-09-17 07:20:46 -0700

    lts-check-last-commit.sh: fix errors
    1. When retrieving job information with curl,
       a 301 redirect error may occur.
       Add the '-L' option to make curl automatically
       follow redirects.
    2. Cloned repositories are stored as tar archives
       in SHARE_FOLDER.
       Extract them before checking the latest commit time.
    
    Change-Id: I19c0d1b89925f87bbe45eb10b66eae32f39d0355
    Signed-off-by: Arthur She <arthur.she@linaro.org>

--- scripts/lts-check-last-commit.sh
+++ scripts/lts-check-last-commit.sh
@@ -6,12 +6,14 @@ TO_BUILD_FILE="${WORKSPACE}/TO_BUILD"
 JOB_URL=${JOB_URL//http:/https:}
 last_build=$((${BUILD_NUMBER}-1))
 API_URL="${JOB_URL}/${last_build}/api/json"
+echo "List SHARE_FORDER:"
+ls -l ${SHARE_FOLDER}
 if [ ${last_build} -eq 0 ]; then
     echo "This is the first build"
     touch ${TO_BUILD_FILE}
 else
     repos_to_check="trusted-firmware-a tf-a-tests tf-a-ci-scripts"
-    last_build=$(curl -s ${API_URL})
+    last_build=$(curl -sL ${API_URL})
     last_build_ts=$(echo ${last_build} | jq '.timestamp')
     last_build_ts=$((${last_build_ts}/1000))
     last_build_result=$(echo ${last_build} | jq -r '.result')
@@ -21,15 +23,20 @@ else
     else
         for r in ${repos_to_check}
         do
-            pushd ${SHARE_FOLDER}/${r}
-            last_commit_ts=$(git show --no-patch --format=%ct)
+            if [ -f "${SHARE_FOLDER}/${r}.tar.gz" ]; then
+                tar -xzf ${SHARE_FOLDER}/${r}.tar.gz
+            else
+                echo "Can not find ${r}"
+                exit 1
+            fi
+            last_commit_ts=$(git -C ${r} show --no-patch --format=%ct)
+            rm -rf ${r}
             # if the last commit was not covered by the last build
             # the new build will be proceeded
             if [ ${last_commit_ts} -ge ${last_build_ts} ]; then
                 touch ${TO_BUILD_FILE}
                 break
             fi
-            popd
         done
     fi
 fi
