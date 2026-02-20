71e8eb792d58f0c38a755d6cd5a8b2812624d68b
Chris Kay <chris.kay@arm.com>
2026-02-20 12:18:03 +0000

    fix: sync `clone.sh` vars with Gerrit trigger params
    The underlying issue behind the topic sync defect is that the parameters
    passed by the Gerrit Trigger plugin, and consumed by various jobs and CI
    scripts, are not used to direct the flow of `clone.sh`.
    
    This bug causes downstream jobs to check out the wrong tree, because the
    clone script looks at the topic for the wrong change, typically notices
    there isn't one, and then doesn't apply it to the trees it clones.
    
    This erroneous behaviour surfaced when the "share folder" support was
    removed, as the top-level job *does* typically configure the correct
    clone variables. Even though the incorrect trigger parameters were
    forwarded, the downstream jobs would still receive the correct tree, and
    so long as they didn't attempt to make use of the clone parameters then
    they would appear to work as expected.
    
    Naturally, all bets would have been off in cases where the parameters
    were read, as they would have been out of sync with the cloned tree.
    
    This change introduces the `tf-a-clone-properties` property macro, which
    dynamically overrides the variables ingested by `clone.sh` for the
    active project in jobs which accept Gerrit Trigger parameters.
    
    Change-Id: Ic3f83753668b6a8f9fe7ce11cc2dc41aa408d0fa
    Signed-off-by: Chris Kay <chris.kay@arm.com>

---
+++ scripts/clone-properties.sh
@@ -0,0 +1,73 @@
+#!/usr/bin/env bash
+
+#
+# Print Java-style properties which translate Gerrit trigger parameters into
+# variable names used by `clone.sh`, based on the active project.
+#
+# These properties are injected as environment variables into the job by the
+# EnvInject plugin.
+#
+
+set -eux -o pipefail
+
+declare -A projects=(
+    ["arm-firmware-crates/arm-cci"]="ARM_CCI_GERRIT_PROJECT"
+    ["arm-firmware-crates/arm-ffa"]="ARM_FFA_GERRIT_PROJECT"
+    ["arm-firmware-crates/arm-fvp-base-pac"]="ARM_FVP_BASE_PAC_GERRIT_PROJECT"
+    ["arm-firmware-crates/arm-generic-timer"]="ARM_GENERIC_TIMER_GERRIT_PROJECT"
+    ["arm-firmware-crates/arm-gic"]="ARM_GIC_GERRIT_PROJECT"
+    ["arm-firmware-crates/arm-mhu"]="ARM_MHU_GERRIT_PROJECT"
+    ["arm-firmware-crates/arm-pl011-uart"]="ARM_PL011_UART_GERRIT_PROJECT"
+    ["arm-firmware-crates/arm-pl061"]="ARM_PL061_GERRIT_PROJECT"
+    ["arm-firmware-crates/arm-psci"]="ARM_PSCI_GERRIT_PROJECT"
+    ["arm-firmware-crates/arm-scmi"]="ARM_SCMI_GERRIT_PROJECT"
+    ["arm-firmware-crates/arm-sp805"]="ARM_SP805_GERRIT_PROJECT"
+    ["arm-firmware-crates/arm-sysregs"]="ARM_SYSREGS_GERRIT_PROJECT"
+    ["arm-firmware-crates/arm-tzc"]="ARM_TZC_GERRIT_PROJECT"
+    ["ci/tf-a-ci-scripts"]="CI_GERRIT_PROJECT"
+    ["hafnium/hafnium"]="SPM_GERRIT_PROJECT"
+    ["RF-A/rusted-firmware-a"]="RF_GERRIT_PROJECT"
+    ["rust-spmc/arm-xlat"]="ARM_XLAT_GERRIT_PROJECT"
+    ["rust-spmc/firmware-development-guide"]="ARM_FW_DEV_GUIDE_GERRIT_PROJECT"
+    ["shared/tf-firmware-handoff"]="TF_FIRMWARE_HANDOFF_GERRIT_PROJECT"
+    ["TF-A/tf-a-tests"]="TFTF_GERRIT_PROJECT"
+    ["TF-A/trusted-firmware-a"]="TF_GERRIT_PROJECT"
+    ["TF-M/tf-m-extras"]="TF_M_EXTRAS_GERRIT_PROJECT"
+    ["TF-M/tf-m-tests"]="TF_M_TESTS_GERRIT_PROJECT"
+    ["TF-RMM/tf-rmm"]="RMM_GERRIT_PROJECT"
+)
+
+declare -A refspecs=(
+    ["arm-firmware-crates/arm-cci"]="ARM_CCI_GERRIT_REFSPEC"
+    ["arm-firmware-crates/arm-ffa"]="ARM_FFA_GERRIT_REFSPEC"
+    ["arm-firmware-crates/arm-fvp-base-pac"]="ARM_FVP_BASE_PAC_GERRIT_REFSPEC"
+    ["arm-firmware-crates/arm-generic-timer"]="ARM_GENERIC_TIMER_GERRIT_REFSPEC"
+    ["arm-firmware-crates/arm-gic"]="ARM_GIC_GERRIT_REFSPEC"
+    ["arm-firmware-crates/arm-mhu"]="ARM_MHU_GERRIT_REFSPEC"
+    ["arm-firmware-crates/arm-pl011-uart"]="ARM_PL011_UART_GERRIT_REFSPEC"
+    ["arm-firmware-crates/arm-pl061"]="ARM_PL061_GERRIT_REFSPEC"
+    ["arm-firmware-crates/arm-psci"]="ARM_PSCI_GERRIT_REFSPEC"
+    ["arm-firmware-crates/arm-scmi"]="ARM_SCMI_GERRIT_REFSPEC"
+    ["arm-firmware-crates/arm-sp805"]="ARM_SP805_GERRIT_REFSPEC"
+    ["arm-firmware-crates/arm-sysregs"]="ARM_SYSREGS_GERRIT_REFSPEC"
+    ["arm-firmware-crates/arm-tzc"]="ARM_TZC_GERRIT_REFSPEC"
+    ["ci/tf-a-ci-scripts"]="CI_REFSPEC"
+    ["hafnium/hafnium"]="SPM_REFSPEC"
+    ["RF-A/rusted-firmware-a"]="RFA_REFSPEC"
+    ["rust-spmc/arm-xlat"]="ARM_XLAT_GERRIT_REFSPEC"
+    ["rust-spmc/firmware-development-guide"]="ARM_FW_DEV_GUIDE_GERRIT_REFSPEC"
+    ["shared/tf-firmware-handoff"]="TF_FIRMWARE_HANDOFF_GERRIT_REFSPEC"
+    ["TF-A/tf-a-tests"]="TFTF_GERRIT_REFSPEC"
+    ["TF-A/trusted-firmware-a"]="TF_GERRIT_REFSPEC"
+    ["TF-M/tf-m-extras"]="TF_M_EXTRAS_GERRIT_REFSPEC"
+    ["TF-M/tf-m-tests"]="TF_M_TESTS_GERRIT_REFSPEC"
+    ["TF-RMM/tf-rmm"]="RMM_REFSPEC"
+)
+
+for project in "${!projects[@]}"; do
+    projects["next/${project}"]="${projects["${project}"]}"
+    refspecs["next/${project}"]="${refspecs["${project}"]}"
+done
+
+printf '%s=%s\n' "${projects["${GERRIT_PROJECT:?}"]}" "${GERRIT_PROJECT:?}"
+printf '%s=%s\n' "${refspecs["${GERRIT_PROJECT:?}"]}" "${GERRIT_REFSPEC:?}"
