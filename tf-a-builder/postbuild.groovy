c465f49c30ae857792b6acfd62a70bafa5db9e48
Chris Kay <chris.kay@arm.com>
2026-03-17 21:07:37 +0000

    refactor: simplify builder post-build script
    Simplify the `tf-a-builder` post-build Groovy script by bubbling up
    exceptions and tidying up some logic.
    
    Change-Id: I5cfef11142cf4c1bf52c06e3860e5eb8319d05b7
    Signed-off-by: Chris Kay <chris.kay@arm.com>

--- tf-a-builder/postbuild.groovy
+++ tf-a-builder/postbuild.groovy
@@ -1,59 +1,30 @@
-def htmlLinks = ''
+def lava = manager.getLogMatcher('LAVA URL: (?<url>[^ ]+) LAVA JOB ID: (?<id>\\d+)')
+def tux = manager.getLogMatcher('TuxSuite test ID: (?<id>[A-Za-z0-9]+)')
 
-def lavaMatcher = manager.getLogMatcher('LAVA URL: (?<url>[^ ]+) LAVA JOB ID: (?<id>\\d+)')
-def lavaMatches = lavaMatcher?.matches()
+if (lava != null) {
+    def id = lava.group('id')
+    def url = lava.group('url')
 
-if (lavaMatches) {
-    def id = lavaMatcher.group('id')
-    def url = lavaMatcher.group('url')
+    def icon = 'symbol-pulse-outline plugin-ionicons-api'
 
-    htmlLinks += "<li><strong>LAVA Job:</strong> <a href=\"${url}\">${id}</a> (<a href=\"artifact/lava.log\">log</a>)</li>"
-
-    def resultMatcher = manager.getLogMatcher('LAVA JOB RESULT: (?<result>\\d+)')
-    def resultMatches = resultMatcher?.matches()
-
-    if (!resultMatches || (resultMatches.group('result') != '0')) {
-        manager.buildFailure()
-    }
+    manager.createSummary(icon).appendText("""
+        <p>LAVA validation results: <a href="${url}">${id}</a></p>
+    """, false)
 }
 
-def tuxMatcher = manager.getLogMatcher('TuxSuite test ID: (?<id>[A-Za-z0-9]+)')
-def tuxMatches = tuxMatcher?.matches()
-
-if (tuxMatches) {
-    def id = tuxMatcher.group('id')
-
-    def abbrLen = 19 // Maximum ID length before we abbreviate it
-    def abbrId = (id.length() > 19) ? ('...' + id.substring(19)) : id
-
+if (tux != null) {
     def group = manager.getEnvVariable('TUXSUITE_GROUP')
     def project = manager.getEnvVariable('TUXSUITE_PROJECT')
 
-    if (group && project) {
-        def jobUrl = "https://tuxapi.tuxsuite.com/v1/groups/${group}/projects/${project}/tests/${id}"
-        def logUrl = "https://storage.tuxsuite.com/public/${group}/${project}/tests/${id}/lava-logs.html"
+    assert group
+    assert project
 
-        htmlLinks += "<li><strong>TuxSuite results:</strong> <a href=\"${jobUrl}\">${abbrId}</a> (<a href=\"${logUrl}\">log</a>)</li>"
+    def id = tux.group('id')
+    def url = "https://tuxapi.tuxsuite.com/v1/groups/${group}/projects/${project}/tests/${id}"
 
-        def resultMatcher = manager.getLogMatcher('TuxSuite test result: (?<result>\\d+)')
-        def resultMatches = resultMatcher?.matches()
+    def icon = 'symbol-pulse-outline plugin-ionicons-api'
 
-        if (!resultMatches || (resultMatcher.group('result') != '0')) {
-            manager.buildFailure()
-        }
-    } else {
-        htmlLinks += '<li><strong>TuxSuite results:</strong> <strong style="color: red;">(missing TuxSuite group/project metadata)</strong></li>'
-
-        manager.buildFailure()
-    }
-}
-
-if (htmlLinks) {
-    def color = (manager.getResult() == 'SUCCESS')
-        ? 'green' : 'red'
-
-    manager.createSummary('clipboard.gif').appendText("""
-        <h1 style="color: ${color};">External test summary</h1>
-        <ul>${htmlLinks}</ul>
+    manager.createSummary(icon).appendText("""
+        <p>TuxSuite validation results: <a href="${url}">${id}</a></p>
     """, false)
 }
