02f28e9cc355f58ae3d9eee3b71ab3b7effb21c5
Chris Kay <chris.kay@arm.com>
2026-03-17 16:34:52 +0000

    refactor: share static checks post-build script
    The post-build Groovy script used by the TF-A, RF-A and RF-A crate
    checks are all virtually identical outside of the name of the artifact
    that they attach to the summary.
    
    Combine these three static checks scripts by parametrising the
    refactored TF-A static checks post-build script.
    
    Change-Id: If4a1c03516713c0bf420353e108c2d7fd2ff8d71
    Signed-off-by: Chris Kay <chris.kay@arm.com>

--- tf-a-static-checks/postbuild.groovy
+++ tf-a-static-checks/postbuild.groovy
@@ -1,23 +1,24 @@
-def project = manager.getEnvVariable('GERRIT_PROJECT')
-def logPath = "${project}/static-checks.log"
-
-def html = (manager.getResult() == 'SUCCESS')
+def logPath = manager.getEnvVariable('TF_A_STATIC_CHECKS_LOG_PATH')
+def html = (logPath && (manager.getResult() == 'SUCCESS'))
     ? '<h1 style="color: green;">No errors detected</h1>'
     : '<h1 style="color: red;">Errors detected</h1>'
 
-try {
-    def artifacts = manager.build
-        .getArtifactManager()
-        .root()
+if (logPath) {
+    try {
+        def log = manager.build
+            .getArtifactManager()
+            .root().child(logPath)
 
-    def log = artifacts
-        .child(project)
-        .child('static-checks.log')
+        html += "<pre><code>${log.open().text}</code></pre>"
+    } catch (IOException e) {
+        html += "<p><strong style=\"color: red;\">Internal error:</strong> could not fetch log artifact: <code>${logPath}</code></p>"
+        manager.listener.logger.println("Could not read log artifact (`${logPath}`): ${e.message}")
+    }
+} else {
+    html += '<p><strong style="color: red;">Internal error:</strong> no log artifact found!</p>'
 
-    html += "<pre><code>${log.open().text}</code></pre>"
-} catch (IOException e) {
-    html += "<strong>Could not read static checks log: <code>${logPath}</code></strong>"
-    manager.listener.logger.println("Could not read static checks log (`${logPath}`): ${e.message}")
+    manager.listener.logger.println("Could not read log artifact: ${logPath}")
+    manager.buildFailure()
 }
 
 manager.createSummary('clipboard.gif').appendText(html, false)
