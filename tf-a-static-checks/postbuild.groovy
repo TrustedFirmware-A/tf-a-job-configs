1a1f61226c0408ed26c9bae5be1bd6e5c40c74aa
Chris Kay <chris.kay@arm.com>
2026-03-18 01:15:04 +0000

    refactor: simplify static checks post-build script
    Simplify the `tf-a-static-checks` post-build Groovy script by bubbling
    up exceptions and tidying up some logic.
    
    Change-Id: I03c62e9512f7445a6f052afb1c28ff8bb879fdf0
    Signed-off-by: Chris Kay <chris.kay@arm.com>

--- tf-a-static-checks/postbuild.groovy
+++ tf-a-static-checks/postbuild.groovy
@@ -1,24 +1,12 @@
-def logPath = manager.getEnvVariable('TF_A_STATIC_CHECKS_LOG_PATH')
-def html = (logPath && (manager.getResult() == 'SUCCESS'))
-    ? '<h1 style="color: green;">No errors detected</h1>'
-    : '<h1 style="color: red;">Errors detected</h1>'
+def logPath = manager
+    .getEnvVariable('TF_A_STATIC_CHECKS_LOG_PATH')
 
-if (logPath) {
-    try {
-        def log = manager.build
-            .getArtifactManager()
-            .root().child(logPath)
+def log = manager.build
+    .getArtifactManager()
+    .root().child(logPath)
 
-        html += "<pre><code>${log.open().text}</code></pre>"
-    } catch (IOException e) {
-        html += "<p><strong style=\"color: red;\">Internal error:</strong> could not fetch log artifact: <code>${logPath}</code></p>"
-        manager.listener.logger.println("Could not read log artifact (`${logPath}`): ${e.message}")
-    }
-} else {
-    html += '<p><strong style="color: red;">Internal error:</strong> no log artifact found!</p>'
+def icon = 'symbol-clipboard-outline plugin-ionicons-api'
 
-    manager.listener.logger.println("Could not read log artifact: ${logPath}")
-    manager.buildFailure()
-}
-
-manager.createSummary('clipboard.gif').appendText(html, false)
+manager.createSummary(icon).appendText("""
+    <pre><code>${log.open().text}</code></pre>
+""", false)
